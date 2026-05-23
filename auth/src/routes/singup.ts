c81792df56ad679f02f175b6640e31cd3483587a
R B suraj <rbsuraj432@gmail.com>
2026-05-23 18:51:16 +0530

    adding custom error classes, and consistent response format through error handler middleware

--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -7,17 +9,18 @@ router.post('/api/users/signup',[
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response) => {
+
   const errors = validationResult(req)
 
   if(!errors.isEmpty()){
-    return res.status(400).send({errors: errors.array()})
+    throw new RequestValidationError(errors.array())
   }
 
   const { email, password } = req.body
 
-  console.log('creating a user')
+  throw new DatabaseConnectionError()
   res.status(201).send({email, password})
 
- })
+})
 
 export {router as signupRouter}
